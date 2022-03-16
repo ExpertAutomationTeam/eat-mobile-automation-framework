501dffe81238f6d11f5339b7a006b9250531fba7
nacerhadjsaid <nacer.hadjsaid@gmail.com>
2022-03-16 19:33:07 -0400

    first commit

---
+++ UICatalog/src/test/java/iPhone/ButtonTest.java
@@ -0,0 +1,16 @@
+package iPhone;
+
+import UiCatalogPage.UiCatalog;
+import navigate.NavigateUi;
+import org.openqa.selenium.support.PageFactory;
+
+public class ButtonTest extends NavigateUi {
+
+    //@Test
+    public void navigate()throws InterruptedException{
+        UiCatalog ui = PageFactory.initElements(ad, UiCatalog.class);
+        ui.getButtonPage();
+
+    }
+
+}
